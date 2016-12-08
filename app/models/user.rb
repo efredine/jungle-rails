0eb1ad5340ef16a8654c3cb6c93598374836691a
Eric Fredine <eric.fredine@gmail.com>
2016-12-08 21:23:46 +0000

    Styling and basic error-handling for sign-up form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, :last_name, :email, presence: true
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   # These routes will be for signup. The first renders a form in the browser, the second will
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  post '/signup' => 'users#create'
 
   # these routes are for showing users a login form, logging them in, and logging them out.
   get '/login' => 'sessions#new'

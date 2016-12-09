0151bb1c8fda775ad8e3afc603e2fea7fce3de59
Eric Fredine <eric.fredine@gmail.com>
2016-12-09 22:38:29 +0000

    Reviews controller with error handling.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,13 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    if flash[:review_with_errors]
+      @review = Review.new(flash[:review_with_errors])
+    else
+      @review = Review.new
+    end
+    logger.debug flash.inspect
+    logger.debug @review.inspect
   end
 
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if !@review.save
+      flash[:review_with_errors] = @review
+      flash[:error_messages] = @review.errors.full_messages
+    end
+    redirect_to @product
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
+
+end

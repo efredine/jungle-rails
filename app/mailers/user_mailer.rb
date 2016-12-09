8dbf46f88c32d65d53b83ed0be0734cf29121922
Eric Fredine <eric.fredine@gmail.com>
2016-12-09 00:00:15 +0000

    Wire email confirmation into cart ordering process.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
-  def welcome_email
-
-    mail(to: 'user@somewhere.com', subject: 'Welcome to My Awesome Site', body: "Not much to say.",
-         content_type: "text/html")
-  end
-
   def order_receipt_email(order)
     @order = order
-    mail(to: @order.email, subject: "Confirmation for Order #{@order.id}")
+    mail(to: @order.email, subject: "Confirmation for Order ##{@order.id}")
   end
 end

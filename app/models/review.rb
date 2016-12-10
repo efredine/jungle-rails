96c22212209a86a981f51eabf634e045b9bd101f
Eric Fredine <eric.fredine@gmail.com>
2016-12-10 02:15:52 +0000

    Made ratings more robust - they have to be btween 1 and 5.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :user, :product, :description, :rating, presence: true
+  validates :rating, numericality: {only_integer: true}, inclusion: {in: 1..5, message: "The rating must be between 1 and 5."}
 end

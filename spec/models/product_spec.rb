2f7f9fbc2ecc80844cacb5e0d833d43fdc3b968d
Eric Fredine <eric.fredine@gmail.com>
2016-12-12 20:08:38 +0000

    Product validation tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'money-rails/test_helpers'
 
 RSpec.describe Product, type: :model do
   let(:category) {Category.create(name: 'Stuff')}
@@ -12,9 +13,62 @@ RSpec.describe Product, type: :model do
   end
 
   describe 'Validations' do
-    it 'should be valid with all fields present' do
+
+    it 'should be valid when all fields are present' do
+      expect(product).to be_valid
+      expect(product).to monetize(:price)
+    end
+
+    it 'should be valid when quantity and price are zero' do
+      product.quantity = 0
+      product.price = 0
       expect(product).to be_valid
     end
+
+    it 'should be invalid when name not present' do
+      product.name = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+      product.name = ''
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should be invalid when quantity not present' do
+      product.quantity = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should be invalid when category not present' do
+      product.category = nil
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'should be invalid when price not present' do
+      product_without_price = category.products.new({
+          name: 'product name',
+          description: 'product description',
+          quantity: 10
+        })
+      expect(product_without_price).to_not be_valid
+      expect(product_without_price).to monetize(:price)
+      expect(product_without_price.errors.full_messages).to include("Price is not a number")
+    end
+
+    it 'should be invalid if price is not a number' do
+      product_with_invalid_price = category.products.new({
+          name: 'product name',
+          description: 'product description',
+          quantity: 10,
+          price: 'chicken train'
+        })
+      expect(product_with_invalid_price).to_not be_valid
+      expect(product_with_invalid_price).to monetize(:price)
+      expect(product_with_invalid_price.errors.full_messages).to include("Price is not a number")
+    end
+
   end
 
 end

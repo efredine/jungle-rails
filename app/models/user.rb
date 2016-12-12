0a71d6fc9eeb8b943e98466c7fd2f88ad5c38b61
Eric Fredine <eric.fredine@gmail.com>
2016-12-12 22:57:04 +0000

    Move authentication logic to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 8 }, allow_nil: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
